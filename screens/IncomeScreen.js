72d3fe00fe2c54b10fc0e21e97b7ec552a1167ea
Agastheshwar2006 <72341702+Agastheshwar2006@users.noreply.github.com>
2021-01-06 14:44:10 +0530

    Add files via upload

---
+++ screens/IncomeScreen.js
@@ -0,0 +1,24 @@
+import React,{Component}from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Modal,
+    KeyboardAvoidingView,
+    StyleSheet,
+    TouchableOpacity,
+    Alert,
+    ScrollView} from 'react-native';
+     
+export default class IncomeScreen extends Component{
+    render(){
+  return(
+      <View >
+          <Text>
+              uncomesdfjhgfhggh
+          </Text>
+      </View>
+  )
+    }
+}
+    
